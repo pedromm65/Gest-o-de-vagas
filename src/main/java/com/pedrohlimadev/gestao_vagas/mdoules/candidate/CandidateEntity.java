a9ed8641371bf739c813288ea51a1febaf9b28b4
Pedro Lima <pedromm65@hotmail.com.br>
2023-10-25 15:27:32 -0300

    a

---
+++ src/main/java/com/pedrohlimadev/gestao_vagas/mdoules/candidate/CandidateEntity.java
@@ -0,0 +1,17 @@
+package com.pedrohlimadev.gestao_vagas.mdoules.candidate;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class CandidateEntity {
+
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String description;
+    private String curriculum;
+}
