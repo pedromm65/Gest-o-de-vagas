8c7fc44b419e0cb1139cdf1e4d5995043aaeea64
Pedro Lima <pedromm65@hotmail.com.br>
2023-11-16 06:43:13 -0300

    Refatorando

---
+++ src/main/java/com/pedrohlimadev/gestao_vagas/modules/candidate/dto/AuthCandidateRequestDTO.java
@@ -0,0 +1,4 @@
+package com.pedrohlimadev.gestao_vagas.modules.candidate.dto;
+
+public record AuthCandidateRequestDTO(String username, String password) {
+}
--- src/main/java/com/pedrohlimadev/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/com/pedrohlimadev/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import com.pedrohlimadev.gestao_vagas.exceptions.UserFoundException;
 import com.pedrohlimadev.gestao_vagas.modules.candidate.CandidateEntity;
 import com.pedrohlimadev.gestao_vagas.modules.candidate.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,18 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository
                 .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+
+        var password = this.passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
         return this.candidateRepository.save(candidateEntity);
     }
